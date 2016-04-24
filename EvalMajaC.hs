7a7791e3d19e2f52d43c8994f620ae7c8ba20be8
Maja <maja.zalewska@gmail.com>
2016-04-24 11:19:14 +0200

    added environment for structures

--- EvalMajaC.hs
+++ EvalMajaC.hs
@@ -22,16 +22,17 @@ import Data.Maybe
 
 
 data ExpResult = MajaInt Integer | MajaBool Bool | MajaVoid 
-                 | MajaArray [Loc] Type | MajaTuple [Loc]
+                 | MajaArray [Loc] Type | MajaTuple [Loc] | MajaStruct Var (M.Map Var Loc)
  
 type Loc = Int
 type Var = Ident
 type Store = (M.Map Loc ExpResult, Loc)
 type VEnv = M.Map Var Loc
 type FEnv = M.Map Var Func
-data Func = Func ([DeclVar], [Loc], [Stmt], Exp, Type, FEnv, VEnv) deriving (Eq, Ord, Show)
+type SEnv = M.Map Var [Struct_dec]
+data Func = Func ([DeclVar], [Loc], [Stmt], Exp, Type, FEnv, VEnv, SEnv) deriving (Eq, Ord, Show)
 
-data S = S (FEnv, VEnv, Store) deriving (Eq, Ord)  
+data S = S (FEnv, VEnv, SEnv, Store) deriving (Eq, Ord)  
 
 initialStore :: Store
 initialStore = (M.empty, 0)
@@ -42,8 +43,11 @@ initialVEnv = M.empty
 initialFEnv :: FEnv
 initialFEnv = M.empty
 
+initialSEnv :: SEnv
+initialSEnv = M.empty
+
 initS :: S
-initS = S (initialFEnv, initialVEnv, initialStore)
+initS = S (initialFEnv, initialVEnv, initialSEnv, initialStore)
 
 instance Eq ExpResult where
    (MajaInt i1) == (MajaInt i2) = i1 == i2
@@ -69,15 +73,16 @@ instance Show ExpResult where
    show MajaVoid = show "MajaVoid"
    show (MajaArray a t) = (show a) ++ (show t)
    show (MajaTuple a) = show a
+   show (MajaStruct v m) = show v ++ show m
 
 execProg :: Program -> IO ()
 execProg (Prog p) = execProg' p initS 
             where
-               execProg' [] (S(f,e,st)) = do
+               execProg' [] (S(f,e,s,st)) = do
                                              mapM_ (putStrLn . show) $ M.toList e 
                                              mapM_ (putStrLn . show) $ M.toList $ fst st
-               execProg' (p:ps) (S(f,e,st)) = do
-                                             x <- execStmt p (S(f,e,st))
+               execProg' (p:ps) (S(f,e,s,st)) = do
+                                             x <- execStmt p (S(f,e,s,st))
                                              execProg' ps x 
                                           
 majaBOp :: (Bool -> Bool -> Bool) -> ExpResult -> ExpResult -> ExpResult
@@ -91,38 +96,39 @@ majaNot (MajaBool b) = MajaBool $ not b
 
 getLoc :: MonadState S m => Var -> m Loc
 getLoc v = do
-            S (_, venv, _) <- get
+            S (_, venv, _, _) <- get
             return $ fromMaybe (error $ "<getLoc>: Undefined variable" ++ show v) $ M.lookup v venv 
 
-getLocArr :: MonadState S m => Var -> Int -> m Loc
-getLocArr v i = do
-            S (_, venv, _) <- get
-            (MajaArray arr _) <- getValueFromLoc $ fromMaybe (error $ "<getLocArray>: Undefined variable" ++ show v) $ M.lookup v venv 
-            return $ arr !! i
+getLocAt :: MonadState S m => Var -> Int -> m Loc
+getLocAt v i = do
+            S (_, venv, _, _) <- get
+            exp <- getValueFromLoc $ fromMaybe (error $ "<getLocAtay>: Undefined variable" ++ show v) $ M.lookup v venv 
+            l <- getList exp
+            return $ l !! i
 
 getVar :: MonadState S m => Var -> m ExpResult
 getVar v = do
              loc <- getLoc v
-             S(_, _, (store, _)) <- get
+             S(_, _, _, (store, _)) <- get
              return $ fromMaybe (error "<getVar>: Undefined variable") $ M.lookup loc store 
 
 getValueFromLoc :: MonadState S m => Loc -> m ExpResult
 getValueFromLoc loc = do
-               S(_, _, (store, _)) <- get
+               S(_, _, _, (store, _)) <- get
                return $ fromMaybe (error "<getVar>: Undefined variable") $ M.lookup loc store 
 
 assign :: MonadState S m => ExpResult -> Loc -> m ()
 assign e loc = do
-               S (fenv, venv, (store, l)) <- get
+               S (fenv, venv, senv, (store, l)) <- get
                case M.lookup loc store of
-                  Just expr -> put $ S (fenv, venv, (M.insert loc e store, l))
+                  Just expr -> put $ S (fenv, venv, senv, (M.insert loc e store, l))
                   Nothing -> error ("Location unused")
 
 --newTup :: MonadState S m => [Type] -> Var -> m ()
 newTup t v = do
-                S(fenv, venv, (store, loc)) <- get
+                S(fenv, venv, senv, (store, loc)) <- get
                 (tup, (store', l)) <- assign' [] t (store, (loc+1))
-                put $ S (fenv, (M.insert v loc venv), (M.insert loc tup store', l))
+                put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc tup store', l))
                   where
                      assign' l [] (store, loc) = return (MajaTuple $ reverse l , (store, loc))
                      assign' l (t:ts) (store, loc) =
@@ -133,34 +139,34 @@ newTup t v = do
 
 newVar :: MonadState S m => Type -> Var -> m ()
 newVar t v = do
-            S(fenv, venv, (store, loc)) <- get
+            S(fenv, venv, senv, (store, loc)) <- get
             case t of
-              TInt -> put $ S (fenv, (M.insert v loc venv), (M.insert loc (MajaInt 0) store, (loc + 1)))
-              TBool -> put $ S (fenv, (M.insert v loc venv), (M.insert loc (MajaBool False) store, (loc + 1)))
+              TInt -> put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc (MajaInt 0) store, (loc + 1)))
+              TBool -> put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc (MajaBool False) store, (loc + 1)))
               TTuple l -> newTup l v 
 
 newVarAssign :: MonadState S m => Type -> Var -> ExpResult -> m ()
 newVarAssign t v e = do
-            S(fenv, venv, (store, loc)) <- get
-            put $ S (fenv, (M.insert v loc venv), (M.insert loc e store, (loc + 1)))
+            S(fenv, venv, senv, (store, loc)) <- get
+            put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc e store, (loc + 1)))
             {--case t of
               TInt -> put $ S (fenv, (M.insert v loc venv), (M.insert loc e store, (loc + 1)))
               TBool -> put $ S (fenv, (M.insert v loc venv), (M.insert loc e store, (loc + 1)))
               TTuple -> put $ S (fenv, (M.insert v loc venv), (M.insert loc e --}
 localEnv :: MonadState S m => m t -> m ()
 localEnv f = do
-               S (fenv, venv, store) <- get
+               S (fenv, venv, senv, store) <- get
                exec <- f
-               S (_, _, store') <- get
-               put $ S (fenv, venv, store')
+               S (_, _, _, store') <- get
+               put $ S (fenv, venv, senv, store')
 
-localEnvF :: MonadState S m => m ExpResult -> FEnv -> VEnv  -> m ExpResult
-localEnvF f fenv venv = do
-                           S (fenv', venv', store) <- get
-                           put $ S (fenv, venv, store)
+localEnvF :: MonadState S m => m ExpResult -> FEnv -> VEnv -> SEnv -> m ExpResult
+localEnvF f fenv venv senv = do
+                           S (fenv', venv', senv', store) <- get
+                           put $ S (fenv, venv, senv, store)
                            exec <- f
-                           S (_, _, store') <- get
-                           put $ S (fenv', venv', store')     
+                           S (_, _, _, store') <- get
+                           put $ S (fenv', venv', senv', store')     
                            return exec
 
 assignLocParams :: MonadState S m => [DeclVar] -> m [Loc]
@@ -168,13 +174,13 @@ assignLocParams d = assign' d []
                     where
                         assign' [] l = return $ reverse l
                         assign' ((DVar t v):ds) l = do
-                              S(fenv, venv, (store, loc)) <- get
+                              S(fenv, venv, senv, (store, loc)) <- get
                               case t of
                                  TInt -> do
-                                          put $ S (fenv, venv, (M.insert loc (MajaInt 0) store, (loc + 1)))  
+                                          put $ S (fenv, venv, senv, (M.insert loc (MajaInt 0) store, (loc + 1)))  
                                           assign' ds $ loc : l
                                  TBool -> do
-                                           put $ S (fenv, venv, (M.insert loc (MajaBool False) store, (loc + 1)))
+                                           put $ S (fenv, venv, senv, (M.insert loc (MajaBool False) store, (loc + 1)))
                                            assign' ds $ loc : l
                                  
                                  _ -> assign' ds l
@@ -182,17 +188,18 @@ assignLocParams d = assign' d []
 newFun :: MonadState S m => Type -> Var -> [DeclVar] -> [Stmt] -> Exp -> m()
 newFun t v d s e = do
                   l <- assignLocParams d
-                  S(fenv, venv, (store, loc)) <- get
-                  let f = fix (\f' -> (Func (d, l, s, e, t, (M.insert v f' fenv), venv)))
-                  put $ S (M.insert v f fenv, venv, (store, loc))  
+                  S(fenv, venv, senv, (store, loc)) <- get
+                  let f = fix (\f' -> (Func (d, l, s, e, t, (M.insert v f' fenv), venv, senv)))
+                  put $ S (M.insert v f fenv, venv, senv, (store, loc))  
 
 getFun :: MonadState S m => Var -> m Func
 getFun f = do
-            S (fenv, _, _) <- get
+            S (fenv, _, _, _) <- get
             return $ fromMaybe (error "<getFun>: Undefined variable") $ M.lookup f fenv 
 
+--TODO structs as parameters
 assignFunParams :: (MonadTrans m, MonadState S (m IO)) => Func -> [Exp] -> m IO VEnv 
-assignFunParams (Func (d, l, _, _, _, _, venv)) p = assign' d l p venv
+assignFunParams (Func (d, l, _, _, _, _, venv, senv)) p = assign' d l p venv
    where
       assign' [] _ _ venv = return venv
       assign' ((DVar t v):ds) l (p:ps) venv = do
@@ -210,22 +217,22 @@ assignFunParams (Func (d, l, _, _, _, _, venv)) p = assign' d l p venv
             TRef _ -> do
                         case p of
                            EVar var -> do
-                              lift $ putStrLn $ show var
+--                              lift $ putStrLn $ show var
                               loc <- getLoc var
                               assign' ds l ps (M.insert v loc venv)
                            EArray var e -> do
-                              lift $ putStrLn $ show var
+  --                            lift $ putStrLn $ show var
                               (MajaInt i) <- evalExpM e
-                              loc <- getLocArr var $ fromInteger i
+                              loc <- getLocAt var $ fromInteger i
                               assign' ds l ps (M.insert v loc venv)
                            _ -> error ("<assignFunParams> Wrong usage of reference") 
 
 newArr :: (MonadTrans m, MonadState S (m IO)) => Type -> Var -> Exp -> m IO () 
 newArr t v e = do
-                  S(fenv, venv, (store, loc)) <- get
+                  S(fenv, venv, senv, (store, loc)) <- get
                   x <- evalExpM e
                   (arr, (store', l)) <- assign' [] x (store, (loc+1)) t
-                  put $ S (fenv, (M.insert v loc venv), (M.insert loc arr store', l))
+                  put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc arr store', l))
                   where
                      assign' l 0 (store, loc) t = return ((MajaArray (reverse l) t) , (store, loc))
                      assign' l x (store, loc) t =
@@ -237,8 +244,8 @@ newArr t v e = do
 newArrAssign :: MonadState S m => Type -> Var -> ExpResult -> m()
 newArrAssign t v arr@(MajaArray l typ) = 
                      if t == typ then do
-                        S(fenv, venv, (store, loc)) <- get
-                        put $ S (fenv, (M.insert v loc venv), (M.insert loc arr store, (loc + 1)))
+                        S(fenv, venv, senv, (store, loc)) <- get
+                        put $ S (fenv, (M.insert v loc venv), senv, (M.insert loc arr store, (loc + 1)))
                      else --do type checkera
                         error ("<newArrAssign>: Types of array and init list's don't match")
 
@@ -254,7 +261,6 @@ getList e = case e of
                (MajaArray l _ ) -> return l
                (MajaTuple l) -> return l
                
-
 getElemAt v (MajaInt i) = do
                            var <- getVar v
                            l <- getList var
@@ -266,9 +272,9 @@ assignArrAll e = do
                   where
                      assign' [] l t = return $ MajaArray (reverse l) TInt
                      assign' (e:es) l t = do
-                         S(fenv, venv, (store, loc)) <- get
+                         S(fenv, venv, senv, (store, loc)) <- get
                          x <- evalExpM e
-                         put $ S (fenv, venv, (M.insert loc x store, (loc + 1)))
+                         put $ S (fenv, venv, senv, (M.insert loc x store, (loc + 1)))
                          case x of
                            MajaInt i -> assign' es (loc:l) TInt
                            MajaBool b -> assign' es (loc:l) TBool
@@ -278,9 +284,9 @@ assignTupAll e1 e2 = do
                   where
                      assign' [] l = return $ MajaTuple (reverse l)
                      assign' (e:es) l = do
-                         S(fenv, venv, (store, loc)) <- get
+                         S(fenv, venv, senv, (store, loc)) <- get
                          x <- evalExpM e
-                         put $ S (fenv, venv, (M.insert loc x store, (loc + 1)))
+                         put $ S (fenv, venv, senv, (M.insert loc x store, (loc + 1)))
                          assign' es (loc:l)
 
 ifelse :: ExpResult -> t -> t -> t
@@ -367,9 +373,9 @@ evalExpM (EPreop LogNeg e) = do
                                 return $ majaNot x
 
 evalExpM (EFunkpar (FCall f e)) = do
-                           fun@(Func (d, l, s, ex, _, fenv, venv)) <- getFun f
+                           fun@(Func (d, l, s, ex, _, fenv, venv, senv)) <- getFun f
                            venv <- assignFunParams fun e
-                           x <- localEnvF (execStmts s >> evalExpM ex) fenv venv
+                           x <- localEnvF (execStmts s >> evalExpM ex) fenv venv senv
                            return x
                            where
                               execStmts [] = return ()
